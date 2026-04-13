bde4e1892e56d18061b303c1d14fb7cf10db607e
CawaRUS <161507844+CawaRUS@users.noreply.github.com>
2026-04-13 04:09:29 +0300

    add static

--- scripts/build.bat
+++ scripts/build.bat
@@ -6,19 +6,19 @@ cd ..
 if not exist bin mkdir bin
 
 echo [1/5] Compiling Core: drp.cpp...
-g++ -c src/drp.cpp -Iinclude -o bin/drp.o -O2
+g++ -c src/drp.cpp -Iinclude -o bin/drp.o -O2 -static-libgcc -static-libstdc++ -static
 
 echo [2/5] Building: bin/drp_receiver.exe
-g++ examples/receiver.cpp bin/drp.o -Iinclude -o bin/drp_receiver.exe -lws2_32 -O2
+g++ examples/receiver.cpp bin/drp.o -Iinclude -o bin/drp_receiver.exe -lws2_32 -O2 -static-libgcc -static-libstdc++ -static
 
 echo [3/5] Building: bin/drp_receiver_file.exe
-g++ examples/file_transfer/file_receiver.cpp bin/drp.o -Iinclude -o bin/drp_receiver_file.exe -lws2_32 -O2
+g++ examples/file_transfer/file_receiver.cpp bin/drp.o -Iinclude -o bin/drp_receiver_file.exe -lws2_32 -O2 -static-libgcc -static-libstdc++ -static 
 
 echo [4/5] Building: bin/drp_sender.exe
-g++ examples/sender.cpp bin/drp.o -Iinclude -o bin/drp_sender.exe -lws2_32 -lpthread -O2
+g++ examples/sender.cpp bin/drp.o -Iinclude -o bin/drp_sender.exe -lws2_32 -lpthread -O2 -static-libgcc -static-libstdc++ -static 
 
 echo [5/5] Building: bin/drp_sender_file.exe
-g++ examples/file_transfer/file_sender.cpp bin/drp.o -Iinclude -o bin/drp_sender_file.exe -lws2_32 -lpthread -O2
+g++ examples/file_transfer/file_sender.cpp bin/drp.o -Iinclude -o bin/drp_sender_file.exe -lws2_32 -lpthread -O2 -static-libgcc -static-libstdc++ -static
 
 echo.
 echo [DONE] All targets built in bin/ folder!
