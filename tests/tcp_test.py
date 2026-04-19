1310150125fa7857374a98195d149bfeb32975e2
CawaRUS <161507844+CawaRUS@users.noreply.github.com>
2026-04-19 19:53:02 +0300

    update tcp test

--- tests/tcp_test.py
+++ tests/tcp_test.py
@@ -5,7 +5,7 @@ start_time = time.time()
 try:
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.settimeout(5)
-    s.connect(('127.0.0.1', 9999)) # Попробуй подключиться
+    s.connect(('127.0.0.1', 9999))
     s.sendall(b"CRITICAL COMMAND")
     print(f"TCP Delivered in: {time.time() - start_time:.3f}s")
 except Exception as e:
